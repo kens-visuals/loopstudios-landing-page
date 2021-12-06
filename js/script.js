861260db14b427454a7c10b53c06ff314ee78467
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-12-05 23:47:57 -0800

    fix a11y issues

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ const btn = document.querySelector('.js-btn');
 const nav = document.querySelector('.js-nav');
 const header = document.querySelector('.js-header');
 
-const toggleMenu = function () {
+const toggleMenu = function (e) {
   if (!header.classList.contains('is-visible')) {
     nav.classList.add('is-visible');
+    e.target.setAttribute('aria-expanded', true);
     nav.firstElementChild.classList.add('is-unset');
     header.classList.add('loopstudios__header--mobile', 'is-visible');
   } else {
     nav.classList.remove('is-visible');
+    e.target.setAttribute('aria-expanded', false);
     nav.firstElementChild.classList.remove('is-unset');
     header.classList.remove('loopstudios__header--mobile', 'is-visible');
   }
